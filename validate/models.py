959f615dbcf92360708cf90fa55068b5f6e668c4
feiFI <781198573@qq.com>
2017-08-29 00:16:54 +0800

    '第一次上传‘

--- validate/models.py
+++ validate/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    nickname = models.CharField(max_length=50,blank=True)
+
+    class Meta(AbstractUser.Meta):
+        pass
+
+class Profile(models.Model):
+    nickname = models.CharField(max_length=50,blank=True)
+    user = models.OneToOneField(User)
--- validate/views.py
+++ validate/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse
+from .forms import RegisterForm
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    else:
+        form = RegisterForm()
+    return render(request,'Regi/register.html',context={'form':form})
+
